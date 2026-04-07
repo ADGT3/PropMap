4836a21d1e89ba4296e5817a3092320ef090a56d
ADGT3 <flat76begun@icloud.com>
2026-04-07 10:50:28 +1000

    Add files via upload

--- api/domain-search.js
+++ api/domain-search.js
@@ -12,7 +12,7 @@
  *   DOMAIN_API_KEY=your_key_here
  */
 
-module.exports = async function handler(req, res) {
+export default async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
